1831caace840df85fad0b6472afea93e6bf67e0b
Moh. Sirajul Umam <sirajul.umam6@gmail.com>
2024-06-17 19:34:34 +0700

    Repository dan Service Menampilkan Todolist

---
+++ src/repository/TodoListRepositoryImpl.java
@@ -0,0 +1,24 @@
+package repository;
+
+import entity.Todolist;
+
+public class TodoListRepositoryImpl implements TodoListRepository {
+
+    private Todolist[] data = new Todolist[10];
+
+    @Override
+    public void add(Todolist todolist) {
+        
+    }
+
+    @Override
+    public Todolist[] getAll() {
+        return data;
+    }
+
+    @Override
+    public void remove(Integer number) {
+        
+    }
+
+}
---
+++ src/service/TodoListServiceImpl.java
@@ -0,0 +1,38 @@
+package service;
+
+import entity.Todolist;
+import repository.TodoListRepository;
+
+public class TodoListServiceImpl implements TodoListService {
+
+    private TodoListRepository todoListRepository;
+
+    public TodoListServiceImpl(TodoListRepository todoListRepository) {
+        this.todoListRepository = todoListRepository;
+    }
+
+    @Override
+    public void addTodoList(String todo) {
+        
+    }
+
+    @Override
+    public void removeTodoList(Integer number) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void showTodoList() {
+        Todolist[] model = todoListRepository.getAll();
+        for (int i = 0; i < model.length; i++) {
+            Todolist todo = model[i];
+            int no = i + 1;
+
+            if (todo != null) {
+                System.out.println(no +". "+ todo);
+            }
+        }
+    }
+
+}
