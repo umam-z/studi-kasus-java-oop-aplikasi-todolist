0dea7145a932542d61841a54a807eb8b9d314432
Moh. Sirajul Umam <sirajul.umam6@gmail.com>
2024-06-17 16:03:49 +0700

    Membuat View

---
+++ src/view/TodoListView.java
@@ -0,0 +1,16 @@
+package view;
+
+public class TodoListView {
+    public void showTodoList() {
+
+    }
+
+    public void addTodoList() {
+
+    }
+
+    public void removeTodoList() {
+
+    }
+    
+}
