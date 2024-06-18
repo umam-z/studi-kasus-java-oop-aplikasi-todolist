0d7aa51f8cc1996832fdf4dbe297b0953e7c3c18
Moh. Sirajul Umam <sirajul.umam6@gmail.com>
2024-06-18 13:12:21 +0700

    Test View Menampilkan Todolist

---
+++ src/util/InputUtil.java
@@ -0,0 +1,12 @@
+package util;
+
+import java.util.Scanner;
+
+public class InputUtil {
+    private static Scanner scanner = new Scanner(System.in);
+    public static String input(String info) {
+        System.err.print(info + " : ");
+        String data = scanner.nextLine();
+        return data;
+    }
+}
--- src/view/TodoListView.java
+++ src/view/TodoListView.java
@@ -1,8 +1,38 @@
 package view;
 
-public class TodoListView {
-    public void showTodoList() {
+import service.TodoListService;
+import util.InputUtil;
 
+public class TodoListView {
+    private TodoListService todoListService;
+    
+    public TodoListView(TodoListService todoListService) {
+        this.todoListService = todoListService;
+    }
+
+    public void showTodoList() {
+        while (true) {
+            
+            System.out.println("TODOLIST");
+            todoListService.showTodoList();
+            
+            System.out.println("Menu");
+            System.out.println("1. Tambah");
+            System.out.println("2. Hapus");
+            System.out.println("x. Keluar");
+    
+            String input = InputUtil.input("pilih");
+            if (input.equals("1")) {
+                addTodoList();
+            } else if(input.equals("2")) {
+                removeTodoList();
+            } else if(input.equals("x")) {
+                System.out.println("Bye");
+                break;
+            } else {
+                System.out.println("Pilahan tidak dimengerti");
+            }
+        }
     }
 
     public void addTodoList() {
